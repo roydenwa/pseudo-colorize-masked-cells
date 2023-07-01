89cf826dbd7db89ac694939473df21ed6da71631
roydenwa <royden-wagner@outlook.com>
2023-07-01 19:32:11 +0200

    Format code

--- src/pseudo_colorize_masked_cells/models/pcolor_mae.py
+++ src/pseudo_colorize_masked_cells/models/pcolor_mae.py
@@ -28,17 +28,20 @@ class PcolorMAE(MAE, pl.LightningModule):
         tokens = self.patch_to_emb(patches)
 
         if self.encoder.pool == "cls":
-            tokens = tokens + self.encoder.pos_embedding[:, 1:(num_patches + 1)]
+            tokens = tokens + self.encoder.pos_embedding[:, 1 : (num_patches + 1)]
         elif self.encoder.pool == "mean":
             tokens = tokens + self.encoder.pos_embedding.to(device)
 
         # calculate of patches needed to be masked, and get random indices, dividing it up for mask vs unmasked
         num_masked = int(self.masking_ratio * num_patches)
-        rand_indices = torch.rand(batch, num_patches, device = device).argsort(dim = -1)
-        masked_indices, unmasked_indices = rand_indices[:, :num_masked], rand_indices[:, num_masked:]
+        rand_indices = torch.rand(batch, num_patches, device=device).argsort(dim=-1)
+        masked_indices, unmasked_indices = (
+            rand_indices[:, :num_masked],
+            rand_indices[:, num_masked:],
+        )
 
         # get the unmasked tokens to be encoded
-        batch_range = torch.arange(batch, device = device)[:, None]
+        batch_range = torch.arange(batch, device=device)[:, None]
         tokens = tokens[batch_range, unmasked_indices]
 
         # attend with vision transformer
@@ -48,14 +51,18 @@ class PcolorMAE(MAE, pl.LightningModule):
         decoder_tokens = self.enc_to_dec(encoded_tokens)
 
         # reapply decoder position embedding to unmasked tokens
-        unmasked_decoder_tokens = decoder_tokens + self.decoder_pos_emb(unmasked_indices)
+        unmasked_decoder_tokens = decoder_tokens + self.decoder_pos_emb(
+            unmasked_indices
+        )
 
         # repeat mask tokens for number of masked, and add the positions using the masked indices derived above
-        mask_tokens = repeat(self.mask_token, 'd -> b n d', b = batch, n = num_masked)
+        mask_tokens = repeat(self.mask_token, "d -> b n d", b=batch, n=num_masked)
         mask_tokens = mask_tokens + self.decoder_pos_emb(masked_indices)
 
         # concat the masked tokens to the decoder tokens and attend with decoder
-        decoder_tokens = torch.zeros(batch, num_patches, self.decoder_dim, device=device)
+        decoder_tokens = torch.zeros(
+            batch, num_patches, self.decoder_dim, device=device
+        )
         decoder_tokens[batch_range, unmasked_indices] = unmasked_decoder_tokens
         decoder_tokens[batch_range, masked_indices] = mask_tokens
         decoded_tokens = self.decoder(decoder_tokens)
@@ -71,19 +78,25 @@ class PcolorMAE(MAE, pl.LightningModule):
         recon_loss = F.mse_loss(pred_pixel_values, masked_pcolor_patches)
         ssim_score = structural_similarity_index_measure(
             pred_pixel_values.view(
-                -1, 3, int(self.encoder.image_size * (1 - self.masking_ratio)),
-                int(self.encoder.image_size * (1 - self.masking_ratio))
+                -1,
+                3,
+                int(self.encoder.image_size * (1 - self.masking_ratio)),
+                int(self.encoder.image_size * (1 - self.masking_ratio)),
             ),
             masked_pcolor_patches.view(
-                -1, 3, int(self.encoder.image_size * (1 - self.masking_ratio)),
-                int(self.encoder.image_size * (1 - self.masking_ratio))
-            )
+                -1,
+                3,
+                int(self.encoder.image_size * (1 - self.masking_ratio)),
+                int(self.encoder.image_size * (1 - self.masking_ratio)),
+            ),
         )
 
         return recon_loss, ssim_score
 
     def training_step(self, batch, batch_idx):
-        loss, ssim_score = self.forward(batch["img"], batch["pcolor_img"].view(-1, 3, 384, 384))
+        loss, ssim_score = self.forward(
+            batch["img"], batch["pcolor_img"].view(-1, 3, 384, 384)
+        )
 
         self.log("train_loss", loss, sync_dist=True)
         self.log("train_ssim", ssim_score, sync_dist=True)
@@ -91,7 +104,9 @@ class PcolorMAE(MAE, pl.LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss, ssim_score = self.forward(batch["img"], batch["pcolor_img"].view(-1, 3, 384, 384))
+        loss, ssim_score = self.forward(
+            batch["img"], batch["pcolor_img"].view(-1, 3, 384, 384)
+        )
 
         self.log("val_loss", loss, sync_dist=True)
         self.log("val_ssim", ssim_score, sync_dist=True)
